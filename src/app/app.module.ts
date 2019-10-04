37acebbd3d99c0cb3ca99ff41556bf3e2b61df6d
Carlos Huguez <carlos.huguez@gmail.com>
2019-10-03 17:02:18 -0700

    se introdudo el navbar y el home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule } from '@angular/core'; 
 
+// rutas
+import  { APP_ROUTING } from './app.routes';
+
+//servicios
+
+
+//componentes
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/shared/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { HeoresComponent } from './components/heores/heores.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    AboutComponent,
+    HeoresComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    APP_ROUTING
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
+
 export class AppModule { }
---
+++ src/app/components/heores/heores.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-heores',
+  templateUrl: './heores.component.html',
+  styles: []
+})
+export class HeoresComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
